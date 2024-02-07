002553433e6154825cb65acdae2f24a9d57a98fa
Charlie <carlos65357@gmail.com>
2024-02-07 07:28:34 -0400

    feat(libro): add basic endpoints and basic controller functions

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Libro;
 use Illuminate\Http\Request;
 
 class LibroController extends Controller
 {
-    //
+    public function index() {
+        $libros = Libro::query();
+        if (request('limit')) {
+            return $libros->paginate(request('limit'));
+        }
+
+        return $libros->paginate(10);
+    }
+
+    public function show($id) {
+        return Libro::find($id);
+    }
+
+    public function store(Request $request) {
+        return Libro::create($request->all());
+    }
+
+    public function update(Request $request, $id) {
+        $libro = Libro::findOrFail($id);
+        $libro->update($request->all());
+        return $libro;
+    }
+
+    public function delete(Request $request, $id) {
+        $libro = Libro::findOrFail($id);
+        $libro->delete();
+        return 204;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LibroController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/libros', [LibroController::class, 'index']);
+Route::get('/libros/{id}', [LibroController::class, 'show']);
+Route::post('/libros', [LibroController::class, 'store']);
+Route::put('/libros/{id}', [LibroController::class, 'update']);
+Route::delete('/libros/{id}', [LibroController::class, 'delete']);
